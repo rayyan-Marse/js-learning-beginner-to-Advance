6a5ef6f869701ba66bde3d1fc94331db1f513232
rayyan <rayyansto@gmail.com>
2022-05-10 01:06:29 +0530

    string functions in js

--- current learing/09-Data-Structures-Operators/starter/script.js
+++ current learing/09-Data-Structures-Operators/starter/script.js
@@ -35,3 +35,47 @@ const plane = 'A250';
 console.log(plane[0]);
 
 console.log('rayyan'[0]);
+console.log('faizanShaikh'.length);
+
+console.log(airline.length);
+console.log(airline.indexOf('p'));
+
+
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('r'));
+console.log(airline.indexOf('portugal'));
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+console.log(airline.slice(0, airline.indexOf('')));
+console.log(airline.slice(airline.indexOf('') + 1));
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMidleSeat = (seat) => {
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('you got a seat');
+  else console.log('you got lucky');
+};
+
+checkMidleSeat('12B');
+checkMidleSeat('15C');
+checkMidleSeat('12E');
+
+
+console.log(airline.toLocaleLowerCase());
+console.log(airline.toUpperCase());
+
+const passanger = 'RaYYan';
+const lowerm = passanger.toLocaleLowerCase();
+const correctm = lowerm[0].toUpperCase() + lowerm.slice(1);
+console.log(correctm);
+
+// when you use trim method it clean white spaces as well as \n that showed in below examples.
+const email = 'rayyansssto@gmail.com;';
+const loginEmail = 'rayyansssto@gmail.com  \n';
+
+const normalizedEmail = loginEmail.toLocaleLowerCase().trim();
+
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
